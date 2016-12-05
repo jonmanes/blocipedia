cbd7fb2166e9b9cb5b8e54f3f89bdc959edb0c7f
jon manes <jon.manes@gmail.com>
2016-12-05 11:35:11 -0600

    Added chuck norris facts to site

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+20.times do
+  User.create!(
+  email: Faker::Internet.email,
+  password: Faker::Internet.password
+  )
+end
+users = User.all
+
+100.times do
+  Wiki.create!(
+    title: Faker::Superhero.name,
+    body: Faker::ChuckNorris.fact
+  )
+end
+wikis = Wiki.all
+
+admin = User.create!(
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+
+member = User.create!(
+   email:    'member@example.com',
+   password: 'helloworld'
+ )
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Wiki.count} wikis created"
