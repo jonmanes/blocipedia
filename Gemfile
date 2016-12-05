3264af5566dea37d524b561a94353e5899f664a5
jon manes <jon.manes@gmail.com>
2016-12-05 10:54:02 -0600

    Checkpoint 5

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ gem 'bootstrap-sass'
 gem 'figaro'
 gem 'pry-rails'
 gem 'devise'
+gem 'pundit'
 
 group :development, :test do
   gem 'byebug'
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :wikis
-  
+
+  enum role: [:standard, :premium, :admin]
+
 end
